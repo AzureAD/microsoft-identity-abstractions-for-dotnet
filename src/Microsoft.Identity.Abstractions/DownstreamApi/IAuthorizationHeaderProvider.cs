0e09887e30dfbe3dd28230e3f3028e0b3ee1e312
sruthikeerthi <73967733+sruke@users.noreply.github.com>
2024-06-20 16:43:55 -0700

    Introduces IAuthorizationHeaderProviderExtension (#129)
    * Introduce IAuthorizationHeaderProviderExtension
    
    * Update IAuthorizationHeaderProvider
    
    * Update changelog
    
    * Update ClientSemVer to 6x
    
    ---------
    
    Co-authored-by: Sruthi Keerthi Rangavajhula (from Dev Box) <srangavajhul@microsoft.com>

--- src/Microsoft.Identity.Abstractions/DownstreamApi/IAuthorizationHeaderProvider.cs
+++ src/Microsoft.Identity.Abstractions/DownstreamApi/IAuthorizationHeaderProvider.cs
@@ -21,7 +21,7 @@ namespace Microsoft.Identity.Abstractions
         /// protocols like Pop), and token acquisition options.</param>
         /// <param name="claimsPrincipal">Inbound authentication elements. In a web API, this is usually the result of the
         /// validation of a token. In a web app, this would be information about the signed-in user. This is not useful in
-        /// daemon applications. In Microsoft.Identity.Web you rarely need to provide this parameter as it's infered from the
+        /// daemon applications. In Microsoft.Identity.Web you rarely need to provide this parameter as it's inferred from the
         /// context.</param>
         /// <param name="cancellationToken">Cancellation token.</param>
         /// <returns>A string containing the authorization header, that is protocol and tokens
@@ -48,6 +48,25 @@ namespace Microsoft.Identity.Abstractions
         Task<string> CreateAuthorizationHeaderForAppAsync(
             string scopes,
             AuthorizationHeaderProviderOptions? downstreamApiOptions = null,
+            CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Creates an authorization header for calling a protected web API on behalf of a user or the application.
+        /// </summary>
+        /// <param name="scopes">The scopes for which to request the authorization header. 
+        /// Provide a single scope if the header needs to be created on behalf of an application.</param>
+        /// <param name="options">The <see cref="AuthorizationHeaderProviderOptions"/> containing information about the API
+        /// to be called and token acquisition settings. If not provided, the header will be for a bearer token.</param>
+        /// <param name="claimsPrincipal">Inbound authentication elements. In a web API, this is usually the result of the
+        /// validation of a token. In a web app, this would be information about the signed-in user. This is not useful in
+        /// daemon applications. In Microsoft.Identity.Web you rarely need to provide this parameter as it's inferred from the
+        /// context.</param>
+        /// <param name="cancellationToken">A token to cancel the operation.</param>
+        /// <returns>A string containing the authorization header, such as "Bearer token" or "PoP token".</returns>
+        Task<string> CreateAuthorizationHeaderAsync(
+            IEnumerable<string> scopes,
+            AuthorizationHeaderProviderOptions? options = null,
+            ClaimsPrincipal? claimsPrincipal = null,
             CancellationToken cancellationToken = default);
     }
 }
