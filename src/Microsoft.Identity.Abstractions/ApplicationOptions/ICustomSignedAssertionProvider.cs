bddd56370990fed4353a1deb7c3f97006cb7cb34
JLoze <103777376+JoshLozensky@users.noreply.github.com>
2025-01-28 18:23:26 -0800

    Adding ICustomSignedAssertionProvider (#154)
    * Adding ICustomSignedAssertionProvider

---
+++ src/Microsoft.Identity.Abstractions/ApplicationOptions/ICustomSignedAssertionProvider.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Identity.Abstractions
+{
+    /// <summary>
+    /// Interface to implement loading of a custom signed assertion provider.
+    /// </summary>
+    public interface ICustomSignedAssertionProvider : ICredentialSourceLoader
+    {
+        /// <summary>
+        /// Configuration friendly name of the custom signed assertion provider as it
+        /// will appear in the <see cref="CredentialDescription.CustomSignedAssertionProviderName"/>
+        /// Can be <c>null</c> in which case developers will need to pass-in the fully
+        /// qualified name of the implementing class.
+        /// </summary>
+        abstract string Name { get; }
+    }
+}
