ab0ea45f0a83926ba0d358313b8e5bf685dd8527
Jean-Marc Prieur <jmprieur@microsoft.com>
2025-03-24 13:11:32 -0700

    Update test/Microsoft.Identity.Abstractions.Tests/CredentialDescriptionIdTest.cs
    Co-authored-by: Keegan <keegancaruso@gmail.com>
    Signed-off-by: Jean-Marc Prieur <jmprieur@microsoft.com>

--- test/Microsoft.Identity.Abstractions.Tests/CredentialDescriptionIdTest.cs
+++ test/Microsoft.Identity.Abstractions.Tests/CredentialDescriptionIdTest.cs
@@ -1,3 +1,6 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using Microsoft.Identity.Abstractions;
 using System;
 using System.Collections.Generic;
