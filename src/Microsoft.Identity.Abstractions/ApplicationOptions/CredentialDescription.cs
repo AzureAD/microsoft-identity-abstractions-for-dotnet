5227b1103e99869c2929d9e3af0c8f96a927e138
Jean-Marc Prieur <jmprieur@microsoft.com>
2025-03-24 14:34:35 -0700

    Update src/Microsoft.Identity.Abstractions/ApplicationOptions/CredentialDescription.cs
    Co-authored-by: Keegan <keegancaruso@gmail.com>
    Signed-off-by: Jean-Marc Prieur <jmprieur@microsoft.com>

--- src/Microsoft.Identity.Abstractions/ApplicationOptions/CredentialDescription.cs
+++ src/Microsoft.Identity.Abstractions/ApplicationOptions/CredentialDescription.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Buffers.Text;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
