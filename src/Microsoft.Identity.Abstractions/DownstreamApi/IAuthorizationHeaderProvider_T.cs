5041f06621d9a7f2930287dca1f65f5abdbdb36c
Keegan <Keegan.Caruso@microsoft.com>
2025-04-16 15:11:22 -0700

    Add a new generic IAuthorizationHeaderProvider (#173)
    * Add a new generic IAuthorizationHeaderProvider
    
    This allows the implementor to specialize the return type
    
    ---------
    
    Signed-off-by: Keegan <keegancaruso@gmail.com>
    Co-authored-by: Keegan Caruso <keegancaruso@microsoft.com>

---
+++ src/Microsoft.Identity.Abstractions/DownstreamApi/IAuthorizationHeaderProvider_T.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Identity.Abstractions
+{
+    /// <summary>
+    /// <inheritdoc cref="IAuthorizationHeaderProvider"/>
+    /// </summary>
+    /// <typeparam name="TResult">The result type.</typeparam>
+    public interface IAuthorizationHeaderProvider<TResult>
+    {
+        /// <inheritdoc cref="IAuthorizationHeaderProvider.CreateAuthorizationHeaderAsync"/>
+        /// <param name="downstreamApiOptions">
+        /// The options containing information about the API
+        /// to be called and token acquisition settings.
+        /// Use <see cref="DownstreamApiOptions.Scopes"/> to request scope(s)
+        /// for the authorization header.
+        /// </param>
+        /// <param name="claimsPrincipal">
+        /// <inheritdoc cref="IAuthorizationHeaderProvider.CreateAuthorizationHeaderAsync"/>
+        /// </param>
+        /// <param name="cancellationToken">
+        /// <inheritdoc cref="IAuthorizationHeaderProvider.CreateAuthorizationHeaderAsync"/>
+        /// </param>        
+        Task<TResult> CreateAuthorizationHeaderAsync(
+            DownstreamApiOptions downstreamApiOptions,
+            ClaimsPrincipal? claimsPrincipal = null,
+            CancellationToken cancellationToken = default);
+    }
+}
