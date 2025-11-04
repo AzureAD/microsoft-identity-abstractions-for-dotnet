04bc7637e2c53d19c845b5fc672454fd4678a5a6
Keegan <Keegan.Caruso@microsoft.com>
2025-11-04 11:28:25 -0800

    Refactor credential description ID to use RedactedClientSecret instead of ClientSecret (#224)

--- test/Microsoft.Identity.Abstractions.Tests/CredentialDescriptionIdTest.cs
+++ test/Microsoft.Identity.Abstractions.Tests/CredentialDescriptionIdTest.cs
@@ -13,7 +13,7 @@ namespace Microsoft.Identity.Abstractions.Tests
     public class CredentialDescriptionIdTest
     {
         const string base64encoded = "MIIDDjCCAfagAwIBAgIQIo6M1+XYDbtDgY/Z3VcWOjANBgkqhkiG9w0BAQsFADAaMRgwFgYDVQQDDA9UZXN0Q2VydGlmaWNhdGUwHhcNMjUwMzI1MDExMTExWhcNMjYwMzI1MDEzMTExWjAaMRgwFgYDVQQDDA9UZXN0Q2VydGlmaWNhdGUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQClI8k4uhi7yfdHHYq17MbWmGl2sdgTZ+yRserSQFLcAoVLkVppfwEeccuj6DQraQXhoXXLVk+DXgTx5+/pJTekNpv4arexGwTG2GztPDaiku188minJVDHB7sD6MqTBXqDrFMfEu4uZZmDRMTC3q+sNhiDpMG09NjH/OZnhab0ORu0lXDxoFKtZbYc1yK56eMsYqz2T+K8JkJejvMtuOq3ZERQSvhn+Ud400wUYTyi45D/5zKHrqw0Xyg9Q7Hl4g7GnBcWPWG8JGIseaLJFWyl5yXEQrR1s7S1RNmX+9/YbSV/JzSzicmM66aytwjI8VTLuoXEXXYdzZo0ZO0oxheRAgMBAAGjUDBOMA4GA1UdDwEB/wQEAwIFoDAdBgNVHSUEFjAUBggrBgEFBQcDAgYIKwYBBQUHAwEwHQYDVR0OBBYEFNQtVk+KmmYHqcdsH/jf305p/dh8MA0GCSqGSIb3DQEBCwUAA4IBAQBmUdRF8R3i3WJiEnxYSpgsdMZox1WrEpQugnLYEywmXqNify/M4qNvBpKVNOVVFmGg94V57D8SHbr77nIhyiwPUF3161MixJ+EO0bl+Iepu3GAQ6nbgnRtsbPhhmFIR5MWCQ7ekVaoxIbvWV/Jxha4ZoVyxLOcshRQaKfUZuzjZorhY0P4oXM7zVAw80btbP5nC5FucH0Z2kzIVwXDJsTKWeBXDSXm67hZ2wA4+B9NDDtRX6hxTT9wWmo6vlogfwGKUJJp+X6rs4jt59whms6himga0exf5k9A32+o8O8RJr0h/1gN6VWOcZeu+aBWH/ozuB/eG6hZMV0/oKvhbUki";
-        
+
         [Fact]
         public void NullSecretIdTest()
         {
@@ -22,7 +22,7 @@ namespace Microsoft.Identity.Abstractions.Tests
                 SourceType = CredentialSource.ClientSecret,
                 ClientSecret = null
             };
-            Assert.Equal("ClientSecret=", credentialDescription.Id);
+            Assert.Equal("RedactedClientSecret=", credentialDescription.Id);
         }
 
         [Fact]
@@ -42,7 +42,7 @@ namespace Microsoft.Identity.Abstractions.Tests
                                 CertificatePassword = "password"
                             }
                         },
-                        
+
                         // KeyVault
                         {
                             CredentialSource.KeyVault,
@@ -53,7 +53,7 @@ namespace Microsoft.Identity.Abstractions.Tests
                                 KeyVaultCertificateName = "MyCertificate"
                             }
                         },
-                        
+
                         // Base64Encoded
                         {
                             CredentialSource.Base64Encoded,
@@ -63,7 +63,7 @@ namespace Microsoft.Identity.Abstractions.Tests
                                 Base64EncodedValue = "MIIDHzCgegA.....r1n8Ta0="
                             }
                         },
-                        
+
                         // Path
                         {
                             CredentialSource.Path,
@@ -74,7 +74,7 @@ namespace Microsoft.Identity.Abstractions.Tests
                                 CertificatePassword = "password"
                             }
                         },
-                        
+
                         // StoreWithThumbprint
                         {
                             CredentialSource.StoreWithThumbprint,
@@ -85,7 +85,7 @@ namespace Microsoft.Identity.Abstractions.Tests
                                 CertificateThumbprint = "962D129A...D18EFEB6961684"
                             }
                         },
-                        
+
                         // StoreWithDistinguishedName
                         {
                             CredentialSource.StoreWithDistinguishedName,
@@ -96,7 +96,7 @@ namespace Microsoft.Identity.Abstractions.Tests
                                 CertificateDistinguishedName = "CN=WebAppCallingWebApiCert"
                             }
                         },
-                        
+
                         // ClientSecret
                         {
                             CredentialSource.ClientSecret,
@@ -106,7 +106,7 @@ namespace Microsoft.Identity.Abstractions.Tests
                                 ClientSecret = "MyClientSecret"
                             }
                         },
-                        
+
                         // SignedAssertionFromManagedIdentity
                         {
                             CredentialSource.SignedAssertionFromManagedIdentity,
@@ -116,7 +116,7 @@ namespace Microsoft.Identity.Abstractions.Tests
                                 ManagedIdentityClientId = "12345"
                             }
                         },
-                        
+
                         // SignedAssertionFilePath
                         {
                             CredentialSource.SignedAssertionFilePath,
@@ -126,7 +126,7 @@ namespace Microsoft.Identity.Abstractions.Tests
                                 SignedAssertionFileDiskPath = "c:/path.signedAssertion"
                             }
                         },
-                        
+
                         // SignedAssertionFromVault
                         {
                             CredentialSource.SignedAssertionFromVault,
@@ -136,7 +136,7 @@ namespace Microsoft.Identity.Abstractions.Tests
                                 KeyVaultCertificateName = "somename"
                             }
                         },
-                        
+
                         // AutoDecryptKeys
                         {
                             CredentialSource.AutoDecryptKeys,
@@ -153,7 +153,7 @@ namespace Microsoft.Identity.Abstractions.Tests
                                 }
                             }
                         },
-                        
+
                         // CustomSignedAssertion
                         {
                             CredentialSource.CustomSignedAssertion,
